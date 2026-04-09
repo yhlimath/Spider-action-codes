11f1ee44c45a8bf603246bc9c881e166ae80121f
yhlimath <98458513+yhlimath@users.noreply.github.com>
2026-04-09 11:42:03 +0200

    Update dtl_transfer_matrix.py

--- dilute_temperley_lieb/dtl_transfer_matrix.py
+++ dilute_temperley_lieb/dtl_transfer_matrix.py
@@ -234,7 +234,7 @@ def analyze_transfer_matrix(L, j):
     print(f"\nSaved analysis outputs to {out_json} and {out_m}")
 
 if __name__ == "__main__":
-    L=6; j=3
+    L=8; j=2
     if len(sys.argv) > 2:
         L = int(sys.argv[1])
         j = int(sys.argv[2])
