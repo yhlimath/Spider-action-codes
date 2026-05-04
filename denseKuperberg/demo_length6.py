06fa53dacd2f4009702a22e9b0fbd736b8292ab6
yhlimath <98458513+yhlimath@users.noreply.github.com>
2026-05-04 18:28:55 +0200

    up

--- denseKuperberg/demo_length6.py
+++ denseKuperberg/demo_length6.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from denseKuperberg.states import generate_paths
 from denseKuperberg.algebra import action_E_i, action_H_i
 
